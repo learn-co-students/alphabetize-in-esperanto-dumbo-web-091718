0673d5d7b1d9c7efd01d1c88d4228c9060f43bdb
Nasser Sanou <nassersanou23@gmail.com>
2018-09-04 04:48:31 +0000

    Completed

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,23 +1,14 @@
 def alphabetize(arr)
 esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 organized = ""
-ezperansza_alphabet = esperanto.split('')
 
 
-# i = 0
-# while i < (alpha.length - 1) 
-# p i
 arr.sort_by do  |word|
-    word.split().collect do | check_letter| ezperansza_alphabet.index(check_letter)
- 
-      # if word[0] == alpha[i]
-
+    word.split('').map do |letter| ezperansza_alphabet = esperanto.split('').index(letter)
       end 
     end 
-#  end 
-#  i += 1
+
  end 
- alphabetize(arr)
  
 #   *** Possible Solution ***
 #  if word[0] == alphabet.index(i) if smae letter match 
