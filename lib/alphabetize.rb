b21844d4b7684500b02e04770ae34318aff0eb46
Nasser Sanou <nassersanou23@gmail.com>
2018-09-04 04:49:02 +0000

    Completed

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,6 @@ def alphabetize(arr)
 esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 organized = ""
 
-
 arr.sort_by do  |word|
     word.split('').map do |letter| ezperansza_alphabet = esperanto.split('').index(letter)
       end 
