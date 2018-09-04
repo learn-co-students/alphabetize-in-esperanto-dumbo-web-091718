5229dc2e7eefa8c35a6c584f14a1687b4b0986b2
Nasser Sanou <nassersanou23@gmail.com>
2018-09-04 04:30:40 +0000

    in progress..

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,22 @@
 def alphabetize(arr)
-  # code here
-end
+alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+organized = ""
+ezperansza_alphabet = alpha.split('')
+
+
+alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+# i = 0
+# while i < (alpha.length - 1) 
+# p i
+arr.sort_by do  |word|
+    word.split().collect do | check_letter| ezperansza_alphabet.index(check_letter)
+ 
+      # if word[0] == alpha[i]
+
+      end 
+    end 
+#  end 
+#  i += 1
+ end 
+ alphabetize(arr)
