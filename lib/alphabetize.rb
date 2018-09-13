6527cf007b24aba854025dda95659baacea7b196
Cristina Murillo <cmurill1@jhu.edu>
2018-09-13 22:48:20 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+  esperanto_sorter = {}
+  esp_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  
+  esp_alphabet.each_char { |letter|
+    esperanto_sorter[letter] = esp_alphabet.index(letter)
+  }
+  
+  arr.sort_by { |sentence|
+     num_arr = sentence.split("").collect { |letter|
+       esperanto_sorter[letter]
+     }
+     
+  }
+  
 end
